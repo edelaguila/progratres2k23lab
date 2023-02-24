b6bc997d547aa3037074d7cfff4d6fd5cce09f1e
CarlosSandoval2003 <jawicho@outlook.com>
2023-02-24 17:35:30 -0600

    Correcciones Iniciales del Proyecto

--- LaboratorioU/Equipo3/MVCUniversidadEquipo3/src/main/java/modelo/Conexion.java
+++ LaboratorioU/Equipo3/MVCUniversidadEquipo3/src/main/java/modelo/Conexion.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package modelo;
+    package modelo;
 
-/**
- *
- * @author Javier
- */
+import java.sql.*;
+
 public class Conexion {
+    private static final String JDBC_URL = "jdbc:mysql://localhost/proyectop312023?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASS = "123456";
+   
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+    }
+    
+    public static void close(ResultSet rs){
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(PreparedStatement stmt){
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
     
 }
